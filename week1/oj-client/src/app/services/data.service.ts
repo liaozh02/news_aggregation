85fce6f96cdc5dca80f81c51f604b60989d969d9
Zhe Wang <zhewangjoe@gmail.com>
2017-02-27 09:46:03 -0800

    Add week1

---
+++ week1/oj-client/src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Problem } from "../models/problem.model";
+import { PROBLEMS } from "../mock-problems";
+
+@Injectable()
+export class DataService {
+
+  constructor() { }
+
+  getProblems(): Problem[] {
+    return PROBLEMS;
+  }
+
+  getProblem(id: number): Problem {
+    return PROBLEMS.find((problem) => problem.id === id);
+  }
+
+}
