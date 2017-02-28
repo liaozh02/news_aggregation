88d0deb8bd71401897a3189b121a47d4eccd5393
Zhe Wang <zhewangjoe@gmail.com>
2017-02-28 13:58:00 -0800

    Revert "Angluar client"
    This reverts commit 672fb5129565879f6dd552c5ddf87435bd178314.

---
+++ week1/oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Problem } from "../../models/problem.model";
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css']
+})
+export class ProblemListComponent implements OnInit {
+
+  problems: Problem[];
+
+  constructor(@Inject("data") private data) { }
+
+  ngOnInit() {
+    this.getProblems();
+  }
+
+  getProblems(): void {
+    this.problems = this.data.getProblems();
+  }
+
+}
