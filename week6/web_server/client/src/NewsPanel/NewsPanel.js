bb863111cdea50577456311317c214bee1a6c14d
liaozh02 <liaozh02@gmail.com>
2017-04-06 21:36:34 -0500

    add week6

---
+++ week6/web_server/client/src/NewsPanel/NewsPanel.js
@@ -0,0 +1,79 @@
+import './NewsPanel.css'
+import React from 'react'
+import NewsCard from '../NewsCard/NewsCard'
+import _ from 'lodash'
+
+class NewsPanel extends React.Component{
+    constructor(){
+        super();
+        this.state = {news:null};
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadMoreNews();
+        this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+        if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+        console.log('Loading more news');
+        this.loadMoreNews();
+        }
+   }
+
+    loadMoreNews(e) {
+        let request = new Request('http://localhost:3000/news', {
+            method: 'GET',
+            cache: false
+        });
+        fetch(request)
+            .then((res) => res.json())
+            .then((news) => {
+                this.setState({
+                    news: this.state.news? this.state.news.concat(news) : news,
+                });
+            });
+    }
+
+    renderNews() {
+        var news_list = this.state.news.map(function(news) {
+            return(
+        //        <a className="list-group-item" key={news.digest} href="#">
+                  <a className="list-group-item" ref="#">
+                    <NewsCard news={news}/>
+                </a>
+            );
+        });
+
+        return(
+            <div className="container-fluid">
+                <div className="list-group">
+                    {news_list}
+                </div>
+            </div>
+        );        
+    }
+
+    render() {
+        if (this.state.news) {
+            return(
+                <div>
+                    {this.renderNews()}
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                    <div id="msg-app-loading">
+                        Loading
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+
+export default NewsPanel;
